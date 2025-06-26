26df4b7a9ebd0c1c49f94f01a2665ec5a3633b62
Nur2424 <taabaldievnur06@gmail.com>
2025-06-26 16:17:21 +0200

    Create Cardinal.java

---
+++ src/Cardinal.java
@@ -0,0 +1,48 @@
+public class Cardinal {
+    private String name;
+    private int age;
+    private int influence;
+    private int votesReceived;
+    private boolean isCandidate;
+
+    public Cardinal(String name, int age, int influence, boolean isCandidate) {
+        this.name = name;
+        this.age = age;
+        this.influence = influence;
+        this.isCandidate = isCandidate;
+        this.votesReceived = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getInfluence() {
+        return influence;
+    }
+
+    public int getVotesReceived() {
+        return votesReceived;
+    }
+
+    public boolean isCandidate() {
+        return isCandidate;
+    }
+
+    public void receiveVote() {
+        this.votesReceived++;
+    }
+
+    public void resetVotes() {
+        this.votesReceived = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Cardinal " + name + " (Age: " + age + ", Influence: " + influence + ")";
+    }
+}
